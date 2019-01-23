3db1e206b08cd41e4c7b03ece0821915e28825eb
mfocuz <w4x@DESKTOP-MFEV29E.localdomain>
2019-01-23 08:17:56 +0300

    multiple changes

--- k8s_collect.pl
+++ k8s_collect.pl
@@ -2,39 +2,183 @@
 use strict;
 
 use JSON;
+use LWP::UserAgent;
 use Data::Dumper;
 
-my $KUBECTL = "/usr/bin/kubectl";
-$ENV{'KUBECONFIG'} = '/home/appliance/k8s_zabbix/config';
+# CONFIG ################################################
+my $APIURL = 'http://127.0.0.1/zabbix/api_jsonrpc.php';
+my $APIUSER = 'zabbixapi';
+my $APIPASS = 'zabbixapipass';
+my $ZABBIX_SENDER = '/usr/bin/zabbix_sender';
+my $ZABBIX_SERVER = '127.0.0.1';
+my $ZABBIX_PORT = '10051';
+my $KUBECTL = '/usr/bin/kubectl';
+my $TMP_FILE_PATH = '/tmp/send_to_zabbix.data';
 
-my $TYPE = $ARGV[0];
-my $NAME = $ARGV[1];
-my $JSON_PATH = $ARGV[2];
-my @jsonPath = split('\.',$JSON_PATH);
+# TOKENS ################################################
+my $ITEM_REGEXP = '^trap\.k8s\.';
+my $TOKEN_HEALTHCHECK = 'healthcheck';
+#########################################################
 
-my $cmd = "";
-if ($TYPE eq 'pods') {
-    $cmd = "$KUBECTL get pods -o json"
+my $HOSTNAME = $ARGV[0];
+$ENV{'KUBECONFIG'} = $ARGV[1];
+
+#############################################
+# Request to Zabbix API for data to collect #
+#############################################
+my $ID = 0;
+my $AUTH = '';
+my @ITEMS;
+my %COLLECTED_DATA;
+my %TO_ZABBIX;
+
+my $ua = LWP::UserAgent->new();
+$ua-> default_header('Content-Type' => 'application/json-rpc');
+# Auth ######################################
+my $authRequest = {
+    jsonrpc => '2.0',
+    method  => 'user.login',
+    params  => {
+        user        => $APIUSER,
+        password    => $APIPASS,
+    },
+    id      => ++$ID,
+};
+my $authRequestJson = encode_json $authRequest;
+
+my $respAuth = $ua->post($APIURL, Content => $authRequestJson);
+if ($respAuth->is_success) {
+    my $responseJson = decode_json $respAuth->content;
+    $AUTH = $responseJson->{result};
+    die "Zabbix API Authentication Error" if length $AUTH == 0;
+} else {
+    die "Zabbix API HTTP Connection Error";
+}
+# Get item list ##############################
+my $postBody = {
+    jsonrpc => '2.0',
+    method  => 'item.get',
+    params  => {
+        output  => ['key_'],
+        filter  =>  {
+             host => $HOSTNAME,
+        },
+    },
+    id      => ++$ID,
+    auth    => $AUTH
+};
+my $postBodyJson = encode_json $postBody;
+my $respItems = $ua->post($APIURL, Content => $postBodyJson);
+if ($respItems->is_success) {
+    my $responseJson = decode_json $respItems->content;
+    @ITEMS = map {$_->{key_}} @{$responseJson->{result}}
+} else {
+    die "Zabbix API HTTP Connection Error";
 }
 
-my $output = `$cmd`;
-my $outJson = decode_json $output;
+################
+# Collect data #
+################
 
-foreach my $item (@{$outJson->{items}}) {
-    next if ($item->{metadata}->{name} ne $NAME);
-
-    my $value = get_value_by_path($item,\@jsonPath);    
-    print $value;
+# Get data from Kubernetes
+foreach my $kind ('pods', 'nodes', 'services', 'deployments') {
+    my $output = `$KUBECTL get $kind -o json`;
+    my $outJson = decode_json $output;
+    $COLLECTED_DATA{$kind} = $outJson->{items};
 }
 
+# Item keys tokenizer
+foreach my $itemKey (@ITEMS) {
+    my ($kind, $namespace, $name, $path) = $itemKey =~ m/$ITEM_REGEXP(.*)\[(.*),(.*),(.*)\]/;
+    next if (!defined $kind || !defined $name || !defined $path);
+
+    my $value;
+    if ($name eq $TOKEN_HEALTHCHECK) {
+        my (@json) = grep {$_->{metadata}->{namespace} eq $namespace} @{$COLLECTED_DATA{$kind}};
+        $value = health_check_pods(\@json, $kind) if ($kind eq 'pods');
+    } else {
+        my @path = split('\.',$path);
+        my ($json) = grep {$_->{metadata}->{name} eq $name} @{$COLLECTED_DATA{$kind}};
+        $value = get_value_by_path($json, \@path);
+    }
+    $TO_ZABBIX{$itemKey} = $value;
+}
+
+# Send collected data to Zabbix
+send_to_zabbix();
+
+#########
+# Funcs #
+#########
+
+# Health check for pods
+sub health_check_pods {
+    my ($json, $kind) = @_;
+
+    if($kind eq 'pods') {
+        my $inReadyStatus = 0;
+        my $numberOfPods = scalar @$json;
+        foreach my $pod (@$json) {
+            my @ready = grep {$_->{type} eq 'Ready'} @{$pod->{status}->{conditions}};
+            $inReadyStatus++ if ($ready[0]->{status} eq 'True');
+        }
+        ($inReadyStatus == $numberOfPods) ? return "OK" : return "Only $inReadyStatus of $numberOfPods are in Ready state";
+    }
+
+}
+
+# Recursively parse JSON path
 sub get_value_by_path {
     my ($json, $path) = @_;
     
     my $element = shift @$path;
-    
+
+    # Check if expressions exists
+    my ($key,$value);
+    if ($element =~ m/=/) {
+        ($key, $value) = $element =~ m/.+\((.+)=(.+)\)/;
+        $element =~ s/(\w+).*/\1/;
+    }
+
     if (scalar @$path > 0) {
-        get_value_by_path($json->{$element}, $path);
+        
+        if (ref $json->{$element} eq 'ARRAY') {
+            return "Key syntax invalid" if (!defined $key or !defined $value);
+
+            # Search for index set in expression
+            my $index;
+            my @elementsArr = @{$json->{$element}};
+            for(my $i=0; $i<=$#elementsArr; $i++) {
+                if($elementsArr[$i]->{$key} eq $value) {
+                    $index = $i;
+                    last;
+                }
+            }
+
+            # If index not defined, most probably path or expression is wrong
+            if (defined $index) {
+                get_value_by_path($json->{$element}->[$index], $path);
+            } else {
+                return "JSON path is incorrect (inside array)";
+            }
+
+        } elsif(ref $json->{$element} eq 'HASH') {
+
+            get_value_by_path($json->{$element}, $path);
+
+        } else {
+            return "JSON path is incorrect";
+        }
+
     } else {
         return $json->{$element};
     }
+}
+
+sub send_to_zabbix {
+    open(my $fh, '>', $TMP_FILE_PATH);
+    map {print $fh "$HOSTNAME $_ $TO_ZABBIX{$_}\n"} keys %TO_ZABBIX;
+    close $fh;
+    my $ret = system("$ZABBIX_SENDER -z $ZABBIX_SERVER -p $ZABBIX_PORT -i $TMP_FILE_PATH");
+    print $ret;
 }
--- k8s_discovery.pl
+++ k8s_discovery.pl
@@ -5,19 +5,113 @@ use JSON;
 use Data::Dumper;
 
 my $KUBECTL = "/usr/bin/kubectl";
-$ENV{'KUBECONFIG'} = "/home/appliance/k8s_zabbix/config";
+$ENV{'KUBECONFIG'} = $ARGV[0];
 
 my $DISCOVERY = $ARGV[1];
+
+my $result = {
+    data => [],
+};
+
+if ($DISCOVERY eq 'containers') {
+    my $output = `$KUBECTL get pods -o json`;
+    my $outJson = decode_json $output;
+    my $result = discover_containers($outJson);
+
+    #print Dumper($result);
+    print encode_json $result;
+}
 
 if ($DISCOVERY eq 'pods') {
     my $output = `$KUBECTL get pods -o json`;
     my $outJson = decode_json $output;
-    my $result = fillin_discovery_data($outJson);
+    my $result = discover_pods($outJson);
 
+    #print Dumper($result);
     print encode_json $result;
 }
 
-sub fillin_discovery_data {
+if ($DISCOVERY eq 'nodes') {
+    my $output = `$KUBECTL get nodes -o json`;
+    my $outJson = decode_json $output;
+    my $result = discover_nodes($outJson);
+
+    #print Dumper($result);
+    print encode_json $result;
+}
+
+if ($DISCOVERY eq 'services') {
+    my $output = `$KUBECTL get services -o json`;
+    my $outJson = decode_json $output;
+    my $result = discover_services($outJson);
+
+    #print Dumper($result);
+    print encode_json $result;
+}
+
+if ($DISCOVERY eq 'deployments') {
+    my $output = `$KUBECTL get deployments -o json`;
+    my $outJson = decode_json $output;
+    my $result = discover_deployments($outJson);
+
+    #print Dumper($result);
+    print encode_json $result;
+}
+
+sub discover_deployments {
+    my $json = shift;
+
+    my $result = {
+        data => [],              
+    };
+
+    foreach my $item (@{$json->{items}}) {
+        my $discovery = {
+            '{#NAME}' => $item->{metadata}->{name},
+            '{#NAMESPACE}' => $item->{metadata}->{namespace},                                           
+        };
+        push @{$result->{data}},$discovery;                      
+    }
+    return $result;
+}
+
+sub discover_services {
+    my $json = shift;
+
+    my $result = {
+        data => [],              
+    };
+
+    foreach my $item (@{$json->{items}}) {
+        my $discovery = {
+            '{#NAME}' => $item->{metadata}->{name},
+            '{#NAMESPACE}' => $item->{metadata}->{namespace},
+        };
+        push @{$result->{data}},$discovery;
+                      
+    }
+    return $result;
+}
+
+sub discover_nodes {
+    my $json = shift;
+
+    my $result = {
+        data => [],
+    };
+    
+    foreach my $item (@{$json->{items}}) {
+        my $discovery = {
+            '{#NAME}' => $item->{metadata}->{name},
+        };
+        push @{$result->{data}},$discovery;
+    }
+
+    return $result;
+}
+
+
+sub discover_containers {
     my $json = shift;
     
     my $result = {
@@ -25,11 +119,33 @@ sub fillin_discovery_data {
     };
 
     foreach my $item (@{$json->{items}}) {
-        my $discovery = {
-            '{#NAME}' => $item->{metadata}->{name}
-        };
-        push @{$result->{data}},$discovery;
+        foreach my $container (@{$item->{spec}->{containers}}) {
+            my $discovery = {
+                '{#NAME}' => $item->{metadata}->{name},
+                '{#NAMESPACE}' => $item->{metadata}->{namespace},
+                '{#CONTAINER}' => $container->{name},
+            };
+            push @{$result->{data}},$discovery;
+        }
     }
 
     return $result;
 }
+
+sub discover_pods {
+    my $json = shift;
+    
+    my $result = {
+        data => [],                     
+    };
+
+    foreach my $item (@{$json->{items}}) {
+        my $discovery = {
+            '{#NAME}' => $item->{metadata}->{name},
+            '{#NAMESPACE}' => $item->{metadata}->{namespace},                                                                            
+        };
+        push @{$result->{data}},$discovery;
+    }
+    return $result;
+}
+
