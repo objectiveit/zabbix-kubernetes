baf0512db878463f21d563e1ede2b64afbd4d62b
mfocuz <w4x@DESKTOP-MFEV29E.localdomain>
2019-02-04 07:43:38 +0300

    add feature for multiple configs and namespaces

--- k8s_discovery.pl
+++ k8s_discovery.pl
@@ -5,9 +5,10 @@ use JSON;
 use Data::Dumper;
 
 my $KUBECTL = "/usr/bin/kubectl";
-$ENV{'KUBECONFIG'} = $ARGV[0];
+#$ENV{'KUBECONFIG'}
+my @CONFIGS = split /,/,$ARGV[0];
 my $DISCOVERY = $ARGV[1];
-my $NAMESPACE = $ARGV[2] || undef;
+my @NAMESPACES = split /,/,$ARGV[2] || undef;
 my $HOSTNAME = $ARGV[3];
 
 my $ZABBIX_SENDER = '/usr/bin/zabbix_sender';
@@ -19,119 +20,113 @@ my $ZABBIXKEY_NODATA_PODS = 'trap.k8s.discovery.nodata.pods';
 my $ZABBIXKEY_NODATA_CONTAINERS = 'trap.k8s.discovery.nodata.containers';
 my $ZABBIXKEY_NODATA_SERVICES = 'trap.k8s.discovery.nodata.services';
 my $ZABBIXKEY_NODATA_DEPLOYMENTS = 'trap.k8s.discovery.nodata.deployments';
+my $ZABBIXKEY_NODATA_NODES = 'trap.k8s.discovery.nodata.nodes';
 
-if ($DISCOVERY eq 'containers') {
-    my $output = `$KUBECTL get pods -o json -n $NAMESPACE`;
-    my $outJson = decode_json $output;
-    my $result = discover_containers($outJson);
+my $RESULT = {
+    data => [],
+};
+my $ZABBIXKEY;
 
-    (scalar @{$result->{data}} == 0) ? report_to_zabbix($ZABBIXKEY_NODATA_CONTAINERS, 1) : report_to_zabbix($ZABBIXKEY_NODATA_CONTAINERS, 0);
+foreach my $config (@CONFIGS) {
+    $ENV{'KUBECONFIG'} = $config;
+    foreach my $ns (@NAMESPACES) {
+        if ($DISCOVERY eq 'containers') {
+            my $output = `$KUBECTL get pods -o json -n $ns`;
+            my $outJson = decode_json $output;
+            my $result = discover_containers($outJson);
+            
+            $ZABBIXKEY = $ZABBIXKEY_NODATA_CONTAINERS;
 
-    #print Dumper($result);
-    print encode_json $result;
+            #print Dumper($result);
+        }
+        elsif ($DISCOVERY eq 'pods') {
+            my $output = `$KUBECTL get pods -o json -n $ns`;
+            my $outJson = decode_json $output;
+            my $result = discover_pods($outJson);
+
+            $ZABBIXKEY = $ZABBIXKEY_NODATA_PODS;
+            #print Dumper($result);
+        }
+
+        elsif ($DISCOVERY eq 'nodes') {
+            my $output = `$KUBECTL get nodes -o json`;
+            my $outJson = decode_json $output;
+            my $result = discover_nodes($outJson);
+
+            $ZABBIXKEY = $ZABBIXKEY_NODATA_NODES;
+            #print Dumper($result);
+        }
+
+        elsif ($DISCOVERY eq 'services') {
+            my $output = `$KUBECTL get services -o json -n $ns`;
+            my $outJson = decode_json $output;
+            my $result = discover_services($outJson);
+
+            $ZABBIXKEY = $ZABBIXKEY_NODATA_SERVICES;
+            #print Dumper($result);
+        }
+
+        elsif ($DISCOVERY eq 'deployments') {
+            my $output = `$KUBECTL get deployments -o json -n $ns`;
+            my $outJson = decode_json $output;
+            my $result = discover_deployments($outJson);
+
+            $ZABBIXKEY = $ZABBIXKEY_NODATA_SERVICES;
+            #print Dumper($result);
+        }
+        else {
+            die "Only pods,services,deployments and containers are supported";
+        }
+    }
 }
 
-if ($DISCOVERY eq 'pods') {
-    my $output = `$KUBECTL get pods -o json -n $NAMESPACE`;
-    my $outJson = decode_json $output;
-    my $result = discover_pods($outJson);
-
-    (scalar @{$result->{data}} == 0) ? report_to_zabbix($ZABBIXKEY_NODATA_PODS, 1) : report_to_zabbix($ZABBIXKEY_NODATA_PODS, 0);
-
-    #print Dumper($result);
-    print encode_json $result;
-}
-
-if ($DISCOVERY eq 'nodes') {
-    my $output = `$KUBECTL get nodes -o json`;
-    my $outJson = decode_json $output;
-    my $result = discover_nodes($outJson);
-
-    #print Dumper($result);
-    print encode_json $result;
-}
-
-if ($DISCOVERY eq 'services') {
-    my $output = `$KUBECTL get services -o json -n $NAMESPACE`;
-    my $outJson = decode_json $output;
-    my $result = discover_services($outJson);
-
-    (scalar @{$result->{data}} == 0) ? report_to_zabbix($ZABBIXKEY_NODATA_SERVICES, 1) : report_to_zabbix($ZABBIXKEY_NODATA_SERVICES, 0);
-
-    #print Dumper($result);
-    print encode_json $result;
-}
-
-if ($DISCOVERY eq 'deployments') {
-    my $output = `$KUBECTL get deployments -o json -n $NAMESPACE`;
-    my $outJson = decode_json $output;
-    my $result = discover_deployments($outJson);
-
-    (scalar @{$result->{data}} == 0) ? report_to_zabbix($ZABBIXKEY_NODATA_DEPLOYMENTS, 1) : report_to_zabbix($ZABBIXKEY_NODATA_DEPLOYMENTS, 0);
-
-    #print Dumper($result);
-    print encode_json $result;
-}
+# Report to Zabbix and print out discovery data
+(scalar @{$RESULT->{data}} == 0) ? report_to_zabbix($ZABBIXKEY, 1) : report_to_zabbix($ZABBIXKEY, 0);
+print encode_json $RESULT;
 
 sub discover_deployments {
     my $json = shift;
-
-    my $result = {
-        data => [],              
-    };
 
     foreach my $item (@{$json->{items}}) {
         my $discovery = {
             '{#NAME}' => $item->{metadata}->{name},
             '{#NAMESPACE}' => $item->{metadata}->{namespace},                                           
         };
-        push @{$result->{data}},$discovery;                      
+        push @{$RESULT->{data}},$discovery;                      
     }
-    return $result;
+    return;
 }
 
 sub discover_services {
     my $json = shift;
-
-    my $result = {
-        data => [],              
-    };
 
     foreach my $item (@{$json->{items}}) {
         my $discovery = {
             '{#NAME}' => $item->{metadata}->{name},
             '{#NAMESPACE}' => $item->{metadata}->{namespace},
         };
-        push @{$result->{data}},$discovery;
+        push @{$RESULT->{data}},$discovery;
                       
     }
-    return $result;
+    return;
 }
 
 sub discover_nodes {
     my $json = shift;
-
-    my $result = {
-        data => [],
-    };
     
     foreach my $item (@{$json->{items}}) {
         my $discovery = {
             '{#NAME}' => $item->{metadata}->{name},
         };
-        push @{$result->{data}},$discovery;
+        push @{$RESULT->{data}},$discovery;
     }
 
-    return $result;
+    return;
 }
 
 
 sub discover_containers {
     my $json = shift;
-    
-    my $result = {
-        data => [],
-    };
 
     foreach my $item (@{$json->{items}}) {
         foreach my $container (@{$item->{spec}->{containers}}) {
@@ -140,11 +135,11 @@ sub discover_containers {
                 '{#NAMESPACE}' => $item->{metadata}->{namespace},
                 '{#CONTAINER}' => $container->{name},
             };
-            push @{$result->{data}},$discovery;
+            push @{$RESULT->{data}},$discovery;
         }
     }
 
-    return $result;
+    return;
 }
 
 sub discover_pods {
@@ -159,9 +154,9 @@ sub discover_pods {
             '{#NAME}' => $item->{metadata}->{name},
             '{#NAMESPACE}' => $item->{metadata}->{namespace},                                                                            
         };
-        push @{$result->{data}},$discovery;
+        push @{$RESULT->{data}},$discovery;
     }
-    return $result;
+    return;
 }
 
 sub report_to_zabbix {
